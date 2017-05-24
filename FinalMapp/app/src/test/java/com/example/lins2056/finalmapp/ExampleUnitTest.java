df48b2d057ad69f75cf342043d205a7d20fa7c57
lins2056 <LinS2056@sduhsd.lan>
2017-05-24 09:40:42 -0700

    create new project

---
+++ FinalMapp/app/src/test/java/com/example/lins2056/finalmapp/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.lins2056.finalmapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
